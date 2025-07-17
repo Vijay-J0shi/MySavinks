49c17eac5404bd633d3e3e8f0502a020c554be30
Vijay Joshi <123758166+Vijay-J0shi@users.noreply.github.com>
2025-07-17 12:57:54 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,8 +17,8 @@ export const signUp= async(req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT=="production",
-            sameSite:"strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -47,8 +47,8 @@ export const logIn = async(req,res)=>{
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT=="production",
-            sameSite:"strict",
+            secure:true,
+            sameSite:"None",
             maxAge: 7*24*60*60*1000
         })
 
