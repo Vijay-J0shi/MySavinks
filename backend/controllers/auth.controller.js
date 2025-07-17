17aea268632fc75b8bd84a3611b2e0507a65a388
Vijay Joshi <123758166+Vijay-J0shi@users.noreply.github.com>
2025-07-17 13:22:03 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -63,11 +63,16 @@ export const logIn = async(req,res)=>{
     
 }
 
-export const logOut = (req,res)=>{
-    try {
-        res.clearCookie("token")
-        return res.status(200).json({message:"Logout Success"})
-    } catch (error) {
-        return res.status(500).json({message:`logOut Failed ${error}`})
-    }
-}
+export const logOut = (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+    return res.status(200).json({ message: "Logout Success" });
+  } catch (error) {
+    return res.status(500).json({ message: `logOut Failed ${error}` });
+  }
+};
+
